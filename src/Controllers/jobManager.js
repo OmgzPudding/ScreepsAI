bd76114659bc0723f2fda30416aa72573acac99f
OmgzPudding <30932476+OmgzPudding@users.noreply.github.com>
2017-08-17 07:25:56 -0600

    Add files via upload

--- src/Controllers/jobManager.js
+++ src/Controllers/jobManager.js
@@ -19,7 +19,7 @@ var jobManager = {
 			'id': null,		// id of job will double as id of the target (ie. a energy source id)
 			'type': null,	// used to classify job tickets to some extent
 			'spots': 0,		// total # of creeps that can be assigned to this
-			'workers': 0	// current # of creeps assigned
+			'workers': []	// current # and IDs of creeps assigned
 		};
 		return job;
 	},
@@ -27,7 +27,46 @@ var jobManager = {
 	scanJobs: function(room)
 	{
 		// Takes the room as input and scans for new jobs
+		this.scanGeneralJobs(room);
+		this.scanStationaryJob(room);
 	},
+
+	scanGeneralJobs: function(room){
+			// General Jobs include supplying the: spawn, extensions, towers, controller, storage
+
+			var structures = room.find(FIND_STRUCTURES, { 
+					filter: (s) => 
+					{
+						return  (s.structureType == STRUCTURE_EXTENSION ||
+								 s.structureType == STRUCTURE_SPAWN     ||
+								 s.structureType == STRUCTURE_TOWER);
+					}  
+				});
+
+			for (var x = 0; x < structures.length; x++){
+
+				var struc = structures[x];
+
+				if (struc.energy < struc.energyCapacity && room.memory.jobs.generalJobBoard.supply[struc.id]){
+					// Non-full energy, job doesn't currently exist
+					var job = this.jobFactory();
+					job.id = struc.id;
+					job.type = struc.type;
+					job.spots = 1;
+
+					room.memory.jobs.generalJobBoard.supply[job.id] = job;
+				}
+			}
+/*
+			structures = room.find(
+				FIND_MY_STRUCTURES, { filter: (s) => return
+			   (s.structureType == STRUCTURE_STORAGE)}
+			  );*/
+
+
+
+	},
+
 
 	scanStationaryJob: function(room)
 	{
@@ -69,7 +108,7 @@ var jobManager = {
 				job.id = sourceArray[i]['id'];
 				job.type = "mining";
 				job.spots = freePositions;
-				job.workers = totalPositions - freePositions;
+				job.workers = sourceArray[i]['miners'];
 
 				// Store the job in memory (post it)
 				//	will overwrite the previous posting
@@ -84,11 +123,81 @@ var jobManager = {
 	jobExists: function(job)
 	{
 		// Will take a JOB and determine if it is already registered
+		var taken = 0;
+		for (var i = 0; i < job.workers.length; i++){
+			if (job.workers[i])
+				taken++;
+		}
+
+		if (job.spots > taken){
+			//console.log("Manager found free job");
+			return true;
+		}
+		return false;
 	},
 
 	removeJob: function(job)
 	{
 		// Takes a JOB and will remove it from the list
+	},
+
+	claimJob: function(job, creep)
+	{
+		for (var i = 0; i < job.workers.length; i++){
+			if (job.workers[i]){
+				job.workers[i] = creep.id;
+				break;
+			}
+		}
+
+		if (job.type == 'mining'){
+			var sources = creep.room.memory.environment.energySourcesArray;
+
+			for (i in sources){
+				if (sources[i]['id'] == job.id){
+					//Found matching mining source
+					//console.log("Found available job");
+					for (var id in sources[i]['miners']){
+						if (sources[i]['miners']['id']){
+							sources[i]['miners']['id'] = creep.id;
+							creep.memory['target'] = sources[i]['id'];
+							break;
+						}
+					}
+				}
+			}
+
+		}
+
+	},
+
+	getFreeJobCount: function(room)
+	{
+		var total = 0;
+
+		for (var jobType in room.memory.jobs){
+			//console.log("JobType: "+jobType);
+			for (var jobClass in room.memory.jobs[jobType]){
+				//console.log("JobClass: "+jobClass);
+				for (var j in room.memory.jobs[jobType][jobClass]){
+					var job = room.memory.jobs[jobType][jobClass][j];
+					if (job && job.id){
+						//console.log("Found free job: "+job.id);
+						var taken = 0;
+
+						for (var i = 0; i < job.workers; i++){
+							if (job.workers[i])
+								taken++;
+						}
+
+
+						total += (job.spots - taken);
+					}
+				}
+			}
+		}
+
+		return total;
 	}
 
 
